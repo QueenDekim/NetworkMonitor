ecc64ab76b38e76a296691ad7272aea8792d0f75
QueenDekim <85904783+QueenDekim@users.noreply.github.com>
2024-10-14 13:51:55 +0300

    Update network_monitor.py

--- network_monitor.py
+++ network_monitor.py
@@ -4,7 +4,6 @@ import json
 import time
 
 def scan_network(network, ports):
-    # Настройка nmap
     
     nm = nmap.PortScanner()
     
@@ -16,7 +15,6 @@ def scan_network(network, ports):
         print(f"Error during scanning: {e}")
         return
 
-    # Подключение к базе данных
     conn = mysql.connector.connect(
         host='localhost',
         user='username',
@@ -25,7 +23,6 @@ def scan_network(network, ports):
     )
     cursor = conn.cursor()
 
-    # Обработка результатов сканирования
     for host in nm.all_hosts():
         print(f"Found device: {host}")
 
@@ -47,28 +44,23 @@ def scan_network(network, ports):
                 }
                 device_info['ports'].append(port_info)
 
-        # Преобразование информации об устройстве в JSON
         device_info_json = json.dumps(device_info)
 
-        # Проверка наличия записи в базе данных
         cursor.execute("SELECT device_info FROM scans WHERE ip = %s", (host,))
         result = cursor.fetchone()
 
         if result:
-            # Если запись существует, проверяем, изменилась ли информация
             if result[0] != device_info_json:
                 cursor.execute(
                     "UPDATE scans SET status = %s, device_info = %s, timestamp = CURRENT_TIMESTAMP WHERE ip = %s",
                     (status, device_info_json, host)
                 )
         else:
-            # Если записи нет, добавляем новую
             cursor.execute(
                 "INSERT INTO scans (ip, status, device_info) VALUES (%s, %s, %s)",
                 (host, status, device_info_json)
             )
 
-    # Сохранение изменений
     conn.commit()
     cursor.close()
     conn.close()
@@ -80,4 +72,4 @@ if __name__ == "__main__":
     while True:
         scan_network(network, ports)
         print("Waiting for 2 minutes before next scan...")
-        time.sleep(120)  # Задержка на 120 секунд (2 минуты)
+        time.sleep(120)
