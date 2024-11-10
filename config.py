6635ca6d7ff9dbd90e4391f0024d6cf348886588
QueenDekim <85904783+QueenDekim@users.noreply.github.com>
2024-11-10 20:35:03 +0300

    Update config.py

--- config.py
+++ config.py
@@ -14,3 +14,9 @@ FLASK_CONFIG = {
     'PORT': 5000,
     'DEBUG': True
 }
+
+SCAN_CONFIG = {
+    "DEFAULT_NETWORK": "192.168.1.0/24",
+    "DEFAULT_PORTS": "22,80,443",
+    "DEFAULT_INTERVAL": 1.0
+}
