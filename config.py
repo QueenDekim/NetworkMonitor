55c01f4c6b96657238d1a28f4732cbcdecedd254
Dekim <dekim@dekimdev.ru>
2024-11-24 03:45:18 +0300

    modified:   config.py

--- config.py
+++ config.py
@@ -1,18 +1,22 @@
 DB_CONFIG = {
-    "host": "10.10.123.7",
-    "user": "dekim",
-    "password": "sisari00",
-    "database": "network_monitoring"
+    'host': 'localhost',
+    'user': 'user',
+    'password': 'password',
+    'database': 'network_monitoring'
 }
 
 VENV = {
-    "PATH": ".\\venv"
+    'PATH': '.\\venv',
 }
 
-FLASK_CONFIG = {'HOST': '0.0.0.0', 'PORT': 5000, 'DEBUG': True}
+FLASK_CONFIG = {
+    'HOST': '0.0.0.0',
+    'PORT': 5000,
+    'DEBUG': True
+}
 
 SCAN_CONFIG = {
-    "DEFAULT_NETWORK": "10.10.123.0/24",
+    "DEFAULT_NETWORK": "192.168.1.0/24",
     "DEFAULT_PORTS": "22,80,443",
     "DEFAULT_INTERVAL": 1.0
 }
